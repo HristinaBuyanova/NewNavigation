58c9b67e806395ce84d5df023628802b155fedf3
HristinaBuyanova <hristina-a@mail.ru>
2022-11-18 22:42:29 +0300

    Created LogInViewController.swift

---
+++ Navigation/Profile/LogInViewController.swift
@@ -0,0 +1,19 @@
+//
+//  LogInViewController.swift
+//  Navigation
+//
+//  Created by Христина Буянова on 18.11.2022.
+//
+
+import UIKit
+
+class LogInViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}

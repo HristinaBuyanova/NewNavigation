ddf3860e1782771a2084cd6e8d4170d42ff4f3f8
HristinaBuyanova <109470755+HristinaBuyanova@users.noreply.github.com>
2022-12-06 17:47:36 +0300

    Added animated for show photo

--- Navigation/Post/PhotoDisplayViewController.swift
+++ Navigation/Post/PhotoDisplayViewController.swift
@@ -10,58 +10,60 @@ import UIKit
 class PhotoDisplayViewController: UIViewController {
 
     var indexPhotoRow: Int!
-    
+
     lazy var photo: UIImageView = {
         let imagePhoto = UIImageView()
+        imagePhoto.clipsToBounds = true
+        imagePhoto.contentMode = .scaleAspectFit
         imagePhoto.translatesAutoresizingMaskIntoConstraints = false
         imagePhoto.image = UIImage(named: photoGallery[indexPhotoRow].image)
         return imagePhoto
     }()
-    
+
     lazy var buttonClose: UIButton = {
         let button = UIButton()
-        let imageButton = UIImage(named: "xmark")
+        button.setImage(UIImage(systemName: "xmark"), for: .normal)
         button.tintColor = .black
-        button.setBackgroundImage(UIImage(named: "xmark"), for: .normal)
-        button.addTarget(self, action: #selector(closeWindow), for: .touchUpInside)
         button.translatesAutoresizingMaskIntoConstraints = false
+        button.addTarget(self, action: #selector(closeWindow), for: .touchUpInside)
         return button
     }()
-    
-    
+
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         self.navigationItem.setHidesBackButton(true, animated: false)
         self.tabBarController?.tabBar.isHidden = true
         view.backgroundColor = .white
-        
-        self.view.addSubview(photo)
+
         self.view.addSubview(buttonClose)
-        
-        UIView.animate(withDuration: 0.5, delay: 0.3) {
+        self.view.addSubview(photo)
+
+
+        UIView.animate(withDuration: 0.7, delay: 0.2) {
             NSLayoutConstraint.activate([
                 self.photo.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
                 self.photo.centerYAnchor.constraint(equalTo: self.view.centerYAnchor),
-                self.photo.heightAnchor.constraint(equalToConstant: 350),
-                self.photo.widthAnchor.constraint(equalToConstant: 350)
+                self.photo.heightAnchor.constraint(equalToConstant: 400),
+                self.photo.widthAnchor.constraint(equalTo: self.view.widthAnchor, constant: -20)
             ])
             self.photo.layoutIfNeeded()
         }
 
-        
-        UIView.animate(withDuration: 0.3, delay: 0.7) {
+
+        UIView.animate(withDuration: 0.3, delay: 0.8) {
             NSLayoutConstraint.activate([
-                self.buttonClose.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor),
-                self.buttonClose.rightAnchor.constraint(equalTo: self.view.rightAnchor, constant: -20),
-                self.buttonClose.heightAnchor.constraint(equalToConstant: 100),
-                self.buttonClose.widthAnchor.constraint(equalToConstant: 100)
+                self.buttonClose.bottomAnchor.constraint(equalTo: self.photo.topAnchor, constant: -5),
+                self.buttonClose.rightAnchor.constraint(equalTo: self.view.rightAnchor, constant: -10),
+                self.buttonClose.heightAnchor.constraint(equalToConstant: 50),
+                self.buttonClose.widthAnchor.constraint(equalToConstant: 50)
             ])
             self.buttonClose.layoutIfNeeded()
         }
-        
+
     }
-    
+
 
     @objc func closeWindow() {
         let transition = CATransition()
